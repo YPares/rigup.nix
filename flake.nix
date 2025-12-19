5e8f95c67a906c81b07bfa265d252d8b7070dc88
Yves Pares <yves.pares@aletiq.com>
2025-12-19 10:21:52 +0100

    flake.nix: added garnix cache config

--- flake.nix
+++ flake.nix
@@ -1,6 +1,11 @@
 {
   description = "rigup - Build your AI agent's rig";
 
+  nixConfig = {
+    extra-substituters = [ "https://cache.garnix.io" ];
+    extra-trusted-public-keys = [ "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g=" ];
+  };
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     blueprint.url = "github:numtide/blueprint";
