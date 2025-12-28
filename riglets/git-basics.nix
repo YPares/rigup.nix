e3a08c6ced825851cc8124c77ed48ab2e6c1ab14
Yves Pares <yves.pares@aletiq.com>
2025-12-28 23:54:58 +0100

    Add git-basics riglet
    - Created git-basics riglet that imports agent-identity
    - Generates git config with agent name/email from shared config
    - Added to example-rig in rigup.toml

---
+++ riglets/git-basics.nix
@@ -0,0 +1,36 @@
+self:
+{
+  config,
+  pkgs,
+  riglib,
+  ...
+}:
+{
+  imports = [ self.riglets.agent-identity ];
+
+  config.riglets.git-basics = {
+    tools = [ pkgs.git ];
+
+    meta = {
+      description = "Git setup with agent-identity";
+      intent = "base";
+      keywords = [
+        "git"
+        "version-control"
+        "vcs"
+        "dvcs"
+      ];
+      status = "stable";
+      version = "0.1.0";
+      disclosure = "none";
+    };
+
+    config-files = riglib.writeFileTree {
+      git."config" = pkgs.writeText "gitconfig" ''
+        [user]
+        	name = ${config.agent.identity.name}
+        	email = ${config.agent.identity.email}
+      '';
+    };
+  };
+}
