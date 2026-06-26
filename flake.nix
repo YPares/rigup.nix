1f344d93907bdae4165354e7ebc2883faa2d6b5f
Yves Pares <yves.pares@aletiq.com>
2026-06-26 12:28:19 +0200

    README & flake.nix: removed refs to garnix binary cache

--- flake.nix
+++ flake.nix
@@ -1,11 +1,6 @@
 {
   description = "rigup - Build your AI agent's rig";
 
-  nixConfig = {
-    extra-substituters = [ "https://cache.garnix.io" ];
-    extra-trusted-public-keys = [ "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g=" ];
-  };
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     blueprint.url = "github:numtide/blueprint";
